730df427730d58c92707deac7c223195cc69881f
Brums21 <75539238+Brums21@users.noreply.github.com>
2024-06-13 17:39:58 +0100

    implementation with masstransit not working

--- DataProcessorAPI/Program.cs
+++ DataProcessorAPI/Program.cs
@@ -1,11 +1,14 @@
-using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using DataProcessorAPI.Data;
 using DataProcessorAPI.Repository;
 using DataProcessorAPI.Services;
-// using Keycloak.AuthServices.Authentication;
-// using Keycloak.AuthServices.Authorization;
+using Keycloak.AuthServices.Authentication;
+using Keycloak.AuthServices.Authorization;
 using MassTransit;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace DataProcessorAPI
 {
@@ -14,7 +17,9 @@ namespace DataProcessorAPI
         public static void Main(string[] args)
         {
             // Load environment variables from .env file
-            Env.Load("../.env");
+            DotNetEnv.Env.Load(".env");
+
+            Console.WriteLine(Environment.GetEnvironmentVariable("RABBITMQ_HOST"));
             var hostName = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
             if (string.IsNullOrEmpty(hostName))
             {
@@ -23,7 +28,46 @@ namespace DataProcessorAPI
             }
             
             var builder = WebApplication.CreateBuilder(args);
-            
+
+            //open telemetry
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Logging.AddOpenTelemetry(options =>
+            {
+                options.AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri("" + Environment.GetEnvironmentVariable("OTEL_uri"));
+                });
+            });
+
+            static void addResource(ResourceBuilder resourceBuilder)
+            {
+                resourceBuilder.AddService("DataProcessorAPI");
+            }
+
+            builder.Services
+                .AddOpenTelemetry()
+                .ConfigureResource(addResource)
+                .WithTracing(tracerBuilder => tracerBuilder
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddOtlpExporter(opt =>
+                    {
+                        opt.Endpoint = new Uri("" + Environment.GetEnvironmentVariable("OTEL_uri"));
+                    })
+                )
+                .WithMetrics(meterBuilder => meterBuilder
+                    .AddProcessInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddRuntimeInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddOtlpExporter(opt =>
+                    {
+                        opt.Endpoint = new Uri("" + Environment.GetEnvironmentVariable("OTEL_uri"));
+                    })
+            );
+
+
             // Context
             // Database context injection
             var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
@@ -32,24 +76,25 @@ namespace DataProcessorAPI
             var connectionString = $"Data Source={dbHost},8002;Initial Catalog={dbName};User ID=sa;Password={dbPassword};TrustServerCertificate=True;Encrypt=false";
             builder.Services.AddDbContext<MatchDbContext>(options => options.UseSqlServer(connectionString));
             
-            // builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
-            // builder.Services
-            //     .AddAuthorization()
-            //     .AddKeycloakAuthorization(options =>
-            //     {
-            //         options.EnableRolesMapping =
-            //             RolesClaimTransformationSource.ResourceAccess;
-            //         options.RolesResource = $"{builder.Configuration["Keycloak:resource"]}";
-            //     })
-            //     .AddAuthorizationBuilder();
+            builder.Services.AddKeycloakWebApiAuthentication(builder.Configuration);
+            builder.Services
+                .AddAuthorization()
+                .AddKeycloakAuthorization(options =>
+                {
+                    options.EnableRolesMapping =
+                        RolesClaimTransformationSource.ResourceAccess;
+                    options.RolesResource = $"{builder.Configuration["Keycloak:resource"]}";
+                })
+                .AddAuthorizationBuilder();
 
             builder.Services.AddScoped<IMatchRepository, MatchRepository>();
 
+            
             // Add services to the container.
             builder.Services.AddMassTransit(x =>
             {
                 x.SetKebabCaseEndpointNameFormatter();
-
+            
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(new Uri("" + Environment.GetEnvironmentVariable("RabbitMQConnectionURI")), h =>
@@ -57,10 +102,10 @@ namespace DataProcessorAPI
                         h.Username("" + Environment.GetEnvironmentVariable("RabbitUser"));
                         h.Password("" + Environment.GetEnvironmentVariable("RabbitPassword"));
                     });
-
+            
                     cfg.ConfigureEndpoints(context);
                 });
-
+            
             });
             
             builder.Services.AddScoped<MatchService>();
